c1baa4bde858511a7ef634469bf088dfcd86c10e
Marco Bortolotti <marcoborto21@gmail.com>
2026-02-22 21:42:05 +0100

    Add other files

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+# src/main.py
+from data_utils import read_csv, display_data
+
 def main():
-    print("Hello, competition project!")
+    # Read data
+    df = read_csv("data/raw/test_values.csv")
+    
+    # Display data
+    display_data(df)
 
 if __name__ == "__main__":
-    main()gi
+    main()
